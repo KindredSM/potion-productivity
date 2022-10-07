f736b28a926d5449480e226671a65a6b599ddc69
Kindred <KDSM@protonmail.com>
2022-10-07 18:13:51 +0100

    before monty

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,94 @@
 <template>
+  <h1 class="header">Potion</h1>
+  <input type="text" class="search" placeholder="Search Potion..." />
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">to-do</router-link>
+    <router-link to="/about">tomorrow</router-link>
+    <button class="new-page">New page</button>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
+$textColor: #e1e1e1;
+$primaryColor: #1f1f1f;
+$highlightColor: #323232;
+
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;500;700&display=swap");
+
+body {
+  background-color: $primaryColor;
+}
+
+.header,
+.search {
+  font-weight: 500;
+  position: sticky;
+  position: absolute;
+  left: 3%;
+}
+
+.header {
+  font-size: 1.2rem;
+  font-weight: 700;
+}
+.search {
+  background: $highlightColor;
+  outline: none;
+  box-shadow: none;
+  border-radius: 2px;
+  width: 10rem;
+  border: 1px #1f1f1f solid;
+  top: 7%;
+  height: 1.5rem;
+  color: $textColor;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Poppins;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $textColor;
 }
 
 nav {
-  padding: 30px;
+  font-size: 0.8rem;
+  position: sticky;
+  position: absolute;
+  top: 20%;
+  left: 3%;
 
   a {
+    width: 10rem;
+    height: 1.2rem;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    padding: 0.5rem;
+    text-decoration: none;
     font-weight: bold;
-    color: #2c3e50;
+    color: $textColor;
+    font-weight: 500;
 
     &.router-link-exact-active {
-      color: #42b983;
+      background-color: $highlightColor;
+      border-radius: 2px;
+      font-weight: 700;
+      // color: #87b5ff;
     }
+  }
+  .new-page {
+    font-size: 1rem;
+    margin-top: 500px;
+    color: $textColor;
+    width: 8rem;
+    height: 3rem;
+    background-color: $highlightColor;
+    outline: none;
+    box-shadow: none;
+    border: 1px #1f1f1f solid;
+    cursor: pointer;
+    font-weight: 900;
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
+  <textarea class="input_block"></textarea>
 </template>
 
+<style lang="scss">
+$textColor: #e1e1e1;
+$primaryColor: #1f1f1f;
+$highlightColor: #323232;
+
+.input_block {
+  font-family: "Poppins";
+  font-weight: 300;
+  font-size: 1.2rem;
+  padding: 40px;
+  background: none;
+  outline: none;
+  box-shadow: none;
+  border-radius: 2px;
+  width: 50vw;
+  margin-top: 100px;
+  border: 1px #1f1f1f solid;
+  height: 50rem;
+  color: $textColor;
+  resize: none;
+}
+</style>
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
